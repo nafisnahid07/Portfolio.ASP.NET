bc3f9b79d159c4f099cf1af948102606c40af96b
Nafis Nahid <109548212+nafisnahid07@users.noreply.github.com>
2023-05-30 23:15:01 +0530

    Initial

---
+++ My_Portfolio/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPortfolio.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPostDownloadPDF(string filePath)
+        {
+            // Get the physical path of the PDF file
+            string physicalPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", filePath.TrimStart('~').TrimStart('/'));
+
+            // Set the file name that will be shown to the user when downloading
+            string fileName = "Nahid_CV.pdf";
+
+            // Read the file content
+            byte[] fileBytes = System.IO.File.ReadAllBytes(physicalPath);
+
+            // Return the file as a downloadable content
+            return File(fileBytes, "application/pdf", fileName);
+        }
+
+    }
+}
---
+++ My_Portfolio/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $('.filter-button').on('click', function () {
+        var filter = $(this).data('filter');
+
+        $('.filter-button').removeClass('selected');
+        $(this).addClass('selected');
+
+        if (filter === '*') {
+            $('.grid-item').fadeIn();
+        } else {
+            $('.grid-item').fadeOut(0);
+            $('.grid-item' + filter).fadeIn();
+        }
+    });
+});
+/*$(document).ready(function () {
+    new WOW().init();
+
+    $('.testi-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+});*/
